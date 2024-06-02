20f41e2f669c71e15a63f86d6f30d1adf1eb1921
MisterLing <herrling.felix@gmail.com>
2024-06-02 10:56:09 +0200

    created functions rectangles and randomized polygons

--- code/imageProcessing.py
+++ code/imageProcessing.py
@@ -1,9 +1,13 @@
 import os
 import cv2
 import random
+
+import numpy as np
+
 from sampleBubble import create_elipse
 import json
 from datasetPreparation import create_xml
+
 
 def image_generation_test():
     # get image TODO: replace with parameters
@@ -25,9 +29,11 @@ def image_generation_test():
     f = open(path + 'sample1.txt', 'w')
 
     # generate and save bubble versions
-    bubble_count = 5
+    bubble_count = 20
     for i in range(bubble_count):
-        add_ellipsis_and_save(modified_image, path, i, f)
+        # add_ellipsis_and_save(modified_image, path, i, f)
+        # add_rectangle_and_save(modified_image, path, i, f)
+        add_polygon_and_save(modified_image, path, i, f)
     f.close()
 
 
@@ -81,6 +87,88 @@ def add_ellipsis_and_save(image, path, idx, f):
 
     f.write(json.dumps(annotation) + "\n")
 
+
+def add_rectangle_and_save(image, path, idx, f):
+    # image dimensions
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+
+    width_buffer = image_width//10
+    height_buffer = image_height//10
+
+    # randomize upper left point of rectangle
+    x_min = random.randint(0, image_width - width_buffer)
+    y_min = random.randint(0, image_height - height_buffer)
+
+    # define lower right point of rectangle
+    x_max = random.randint(x_min + width_buffer, image_width)
+    y_max = random.randint(y_min + height_buffer, image_height)
+
+    copy = image.copy()
+    outline_thickness = random.randint(2, 7)
+    copy = cv2.rectangle(copy, (x_min, y_min), (x_max, y_max), (0, 0, 0), -1)
+    copy = cv2.rectangle(copy, (x_min + outline_thickness, y_min + outline_thickness),
+                         (x_max - outline_thickness, y_max - outline_thickness), (255, 255, 255), -1)
+
+    # write to file
+    file = 'rect_' + str(idx)
+    file_name = file + '.jpg'
+    cv2.imwrite(path + file_name, copy)
+
+    coordinates = [['speech_bubble', x_min, y_min, x_max, y_max]]
+    create_xml(path=path, img=file, width=image_width, height=image_height, boxes=coordinates)
+
+
+def add_polygon_and_save(image, path, idx, f):
+    # image dimensions
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+
+    # buffer values to avoid speech bubble out of frame
+    x_buffer = 100
+    y_buffer = 200
+
+    # define offsets for position
+    x_offset = random.randint(0, image_width - x_buffer)
+    y_offset = random.randint(0, image_height - y_buffer)
+
+    # define standard polygon
+    basic_polygon = np.array([[100, 100], [140, 130], [160, 200], [140, 240], [100, 270], [60, 240], [40, 200], [60, 130]], np.int32)
+
+    # create copy for randomization
+    curr_polygon = basic_polygon.copy()
+
+    # randomize each point
+    for i, vertex in enumerate(curr_polygon):
+        vertex[0] = vertex[0] + random.randint(-10, 10) + x_offset
+        vertex[1] = vertex[1] + random.randint(-10, 10) + y_offset
+
+    # draw polygon
+    copy = image.copy()
+    cv2.fillPoly(copy,  [curr_polygon], (255, 255, 255))
+    cv2.polylines(copy, [curr_polygon], True, (0, 0, 0), thickness=5)
+
+    # save file
+    file = 'poly_' + str(idx)
+    file_name = file + '.jpg'
+    cv2.imwrite(path + file_name, copy)
+
+    # get inner boundaries for text
+    inner_x_min = max(int(curr_polygon[5][0]), int(curr_polygon[7][0]))
+    inner_y_min = max(int(curr_polygon[1][1]), int(curr_polygon[7][1]))
+    inner_x_max = min(int(curr_polygon[1][0]), int(curr_polygon[3][0]))
+    inner_y_max = min(int(curr_polygon[3][1]), int(curr_polygon[5][1]))
+
+    # get outer boundaries for labeling
+    x_min = max(int(curr_polygon[6][0]), 0)
+    y_min = max(int(curr_polygon[0][1]), 0)
+    x_max = min(int(curr_polygon[2][0]), image_width)
+    y_max = min(int(curr_polygon[4][1]), image_height)
+
+    coordinates = [['speech_bubble', x_min, y_min, x_max, y_max]]
+    create_xml(path=path, img=file, width=image_width, height=image_height, boxes=coordinates)
+
+
 # Common online version, doesn't work that great and only combines images
 # def cartoonize(image):
 #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -107,6 +195,21 @@ def cartoon_test(image, debug_windows=False):
     # convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    contrast = gray.copy()
+    for x in range(len(contrast)):
+        for y in range(len(contrast[x])):
+            if contrast[x][y] < 125:
+                contrast[x][y] = 0
+            else:
+                contrast[x][y] = 255
+
+    # contours
+    contours, hierarchy = cv2.findContours(contrast, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # print(len(contours))
+    cnt = sorted(contours, key=cv2.contourArea, reverse=True)[:200]
+    mask = np.zeros(gray.shape, gray.dtype)
+    masked = cv2.drawContours(mask, cnt, -1, (255, 255, 255), -1)
+
     # blur
     blur = cv2.GaussianBlur(gray, (0, 0), sigmaX=33, sigmaY=33)
 
@@ -131,9 +234,11 @@ def cartoon_test(image, debug_windows=False):
     # display it if debug enabled
     if debug_windows:
         cv2.imshow("gray", gray)
-        cv2.imshow("divide", divide)
+        #cv2.imshow("divide", divide)
         cv2.imshow("thresh", thresh)
-        cv2.imshow("morph", morph)
+        #cv2.imshow("morph", morph)
+        cv2.imshow("contours", masked)
+        cv2.imshow("contrast", contrast)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
